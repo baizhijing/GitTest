39adef25f881f3c4c1cb5194079725fbcf178fa8
植静 白 <842299086@qq.com>
2021-03-30 14:32:40 +0800

    添加项目文件。

---
+++ WinFormsApp0301/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WinFormsApp0301
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.testButtonC = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "我的测试按钮";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnClickB);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // testButtonC
+            // 
+            this.testButtonC.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.testButtonC.Location = new System.Drawing.Point(623, 449);
+            this.testButtonC.Name = "testButtonC";
+            this.testButtonC.Size = new System.Drawing.Size(170, 46);
+            this.testButtonC.TabIndex = 2;
+            this.testButtonC.Text = "button2";
+            this.testButtonC.UseVisualStyleBackColor = true;
+            this.testButtonC.Click += new System.EventHandler(this.testButtonC_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 38);
+            this.textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(47, 703);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 200);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 944);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.testButtonC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "白植静好帅";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button testButtonC;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WinFormsApp0301/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsApp0301.ChildForms;
+
+namespace WinFormsApp0301
+{
+    public partial class Form1 : Form
+    {
+        //private Button testButton = new Button();
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Controls.Add(testButton);
+            //testButton.Text = "帅B";
+            //testButton.Location = new Point(12, 12);
+            this.textBox1.AutoSize = false;
+        }
+
+        private void OnClickB(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void testButtonC_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
